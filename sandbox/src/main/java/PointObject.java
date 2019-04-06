7a436f74fb86688275761de47917ab1d229ae524
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-04-06 20:40:05 +0300

    Explanation added

--- sandbox/src/main/java/PointObject.java
+++ sandbox/src/main/java/PointObject.java
@@ -3,12 +3,12 @@ public class PointObject {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(10, 10);
 
-        System.out.println(p1.distanceFromPoint(p1, p2));
+        System.out.println("Вычисление расстояния между двумя точками (p1 и p2), с помощью метода distanceFromPoint, из класса Point: расстояние равно " + p1.distanceFromPoint(p1, p2));
 
         Point p3 = new Point(2, 2);
         Point p4 = new Point(18, 18);
 
-        System.out.println(distanceFromPointObject(p3, p4));
+        System.out.println("Вычисление расстояния между двумя точками (p3 и p4), с помощью метода distanceFromPointObject, из класса PointObject: расстояние равно " +distanceFromPointObject(p3, p4));
 
     }
 

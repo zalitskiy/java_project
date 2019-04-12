8388452aa8936a869d38b51d9907472a1ecc72af
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-04-13 01:29:09 +0300

    рефакторинг кода
    Параметризация метода заполнения формы.
    Группировка параметров метода заполнения формы в объект, создание вспомогательного "модельного" класса GroupData для представления таких объектов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -29,7 +29,7 @@ public class GroupCreationTest {
     public void testGroupCreation() throws Exception {
         gotoGroupPage();
         initGroupCreation();
-        fillGroupForm();
+        fillGroupForm(new GroupData("testGroupName1", "testGroupName1", "testGroupName1"));
         submitGroupCreation();
         gotoGroupPage();
         logout();
@@ -43,18 +43,16 @@ public class GroupCreationTest {
         wd.findElement(By.name("submit")).click();
     }
 
-    private void fillGroupForm() {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).click();
+    private void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("testGroupName1");
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("testGroupName1");
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("testGroupName1");
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
     }
 
     private void initGroupCreation() {

eeb9f22c116e16fcf4200e627f03c1ab843c5efd
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-04-20 17:33:50 +0300

    Заменил else на else if для IE

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -29,7 +29,7 @@ public class ApplicationManager {
         else if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         }
-        else {
+        else if (browser.equals(BrowserType.IE)){
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);

b6e74ba3852fdbc3ec43915c2a3b3041c33dc896
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-04-26 00:38:56 +0300

    reformat code in Equation class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -6,20 +6,20 @@ public class Equation {
     private double c;
     private int n;
 
-    public Equation (double a, double b, double c) {
+    public Equation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
-        if (d>0) {
-            n=2;
+        if (d > 0) {
+            n = 2;
         } else {
-            if (d==0) {
-                n=1;
+            if (d == 0) {
+                n = 1;
             } else {
-                n=0;
+                n = 0;
             }
         }
     }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -6,19 +6,19 @@ import org.testng.annotations.Test;
 public class EquationTests {
     @Test
     public void test0() {
-        Equation e = new Equation(1,1,1);
+        Equation e = new Equation(1, 1, 1);
         Assert.assertEquals(e.getNumber(), 0);
     }
 
     @Test
     public void test1() {
-        Equation e = new Equation(1,2,1);
+        Equation e = new Equation(1, 2, 1);
         Assert.assertEquals(e.getNumber(), 1);
     }
 
     @Test
     public void test2() {
-        Equation e = new Equation(1,5,6);
+        Equation e = new Equation(1, 5, 6);
         Assert.assertEquals(e.getNumber(), 2);
     }
 }

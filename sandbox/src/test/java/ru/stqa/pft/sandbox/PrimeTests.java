9c4b083dbeef05a026fae8dc7adab2a55c84604a
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-04-27 01:20:42 +0300

    добавлен класс Primes и тесты для него

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+    @Test
+    public void testPrimes() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+    @Test
+    public void testPrimesFast() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
+    @Test(enabled = false)
+    public void testPrimesLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+        @Test
+    public void testNonPrimes() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+}

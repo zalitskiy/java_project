c2a09bb6344e514c5faa26f684d29a2376a417c1
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-05-09 15:54:18 +0300

    new class for learning

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,8 +13,20 @@ public class Collections {
 
         List<String> arr = Arrays.asList("java", "php", "python", "c#");
 
-        for (String l: arr) {
+        for (String l : arr) {
             System.out.println("Я хочу выучить " + l);
         }
+
+        List<String> list = new ArrayList<>();
+        list.add("hop");
+        list.add("hey");
+        list.add("la-la-ley");
+        list.add("hop");
+        System.out.println(list.get(0));
+        System.out.println(list.get(2));
+
+
+        Assert.assertEquals(list.get(0), list.get(3));
+
     }
 }

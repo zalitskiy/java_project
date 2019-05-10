57295af4028c2a7b0eda44ef651db97aa4f20128
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-05-10 21:55:54 +0300

    Revert "new module added"
    This reverts commit f88371ee25d48021c9e4f0fc624438c7f08de57b.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -8,24 +8,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Collections {
-    @Test
-    public void testMethod1() throws Exception {
+    public static void main(String[] args) {
         String[] langs = {"java", "php", "python", "c#"};
-        for (int i = 0; i < langs.length; i++) {
-            System.out.println(langs[i]);
-        }
-    }
 
-    @Test
-    public void testMethod2() throws Exception {
         List<String> arr = Arrays.asList("java", "php", "python", "c#");
+
         for (String l : arr) {
             System.out.println("Я хочу выучить " + l);
         }
-    }
-    @Test
-    public void testMethod3() throws Exception {
-    List<String> list = new ArrayList<>();
+
+        List<String> list = new ArrayList<>();
         list.add("hop");
         list.add("hey");
         list.add("la-la-ley");
@@ -33,6 +25,8 @@ public class Collections {
         System.out.println(list.get(0));
         System.out.println(list.get(2));
 
-        Assert.assertEquals(list.get(0),list.get(2));
+
+        Assert.assertEquals(list.get(0), list.get(3));
+
     }
 }

98151a167388e4f392d20666bf25ea9e90a82813
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-05-12 20:11:15 +0300

    созданы конфигурационные файлы testng

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
@@ -7,13 +7,11 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 

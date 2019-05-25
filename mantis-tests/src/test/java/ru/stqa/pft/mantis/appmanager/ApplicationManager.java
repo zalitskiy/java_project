a9afabf3f9ad372cafb22f1fe7586be0c9f01bca
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-05-26 01:45:45 +0300

    Работа над тестами по восьмому модулю

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -15,9 +14,9 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private final Properties properties;
-    WebDriver wd;
-
+    private WebDriver wd;
     private String browser;
+    private RegistrationHelper registrationHelper;
 
 
     public ApplicationManager(String browser) {
@@ -28,23 +27,41 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-
-        if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
-
-    public void logout() {
-        wd.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/FtpHelper.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FtpHelper {
+    private final ApplicationManager app;
+    private FTPClient ftp;
+
+    public FtpHelper(ApplicationManager app) {
+        this.app = app;
+        ftp = new FTPClient();
+    }
+
+    public void upload(File file, String target, String backup) throws IOException {
+        ftp.connect(app.getProperty("ftp.host"));
+        ftp.login(app.getProperty("ftp.login"), app.getProperty("ftp.password"));
+    }
+}

c0fdbcf87da4ed4837047d8e4c8a91c08b28d5d1
zalitskiy <49170975+zalitskiy@users.noreply.github.com>
2019-06-08 18:15:52 +0300

    Update ApplicationManager.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -34,7 +34,7 @@ public class ApplicationManager {
     }
 
     public void init() throws IOException {
-        String target = System.getProperty("target", "local");
+        String target = System.getProperty("target", "remote");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
